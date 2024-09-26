764e847b1117ba129823ef1c63f5ec9a9c8173f8
peterberekvolgyi <berekvolgyipeter@gmail.com>
2024-09-26 21:32:19 +0200

    implement ERC20 token with openzeppelin

---
+++ src/OurToken.sol
@@ -0,0 +1,10 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.27;
+
+import {ERC20} from "openzeppelin/token/ERC20/ERC20.sol";
+
+contract OurToken is ERC20 {
+    constructor(uint256 initialSupply) ERC20("OurToken", "OT") {
+        _mint(msg.sender, initialSupply);
+    }
+}
